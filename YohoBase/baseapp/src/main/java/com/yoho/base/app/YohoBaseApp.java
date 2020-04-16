5425d16cb8305958cd8b92bb40b3aea545cc2ac2
yoho <wangpuyong@boco.com.cn>
2020-04-16 17:48:33 +0800

    Initial commit

---
+++ YohoBase/baseapp/src/main/java/com/yoho/base/app/YohoBaseApp.java
@@ -0,0 +1,14 @@
+package com.yoho.base.app;
+
+import android.app.Application;
+
+/**
+ * 类名
+ *
+ * @author Yoho https://github.com/YohoElf
+ * @description 类功能内容
+ * @date Created in 2020-04-16 17:14
+ * @modified By Yoho
+ */
+public class YohoBaseApp extends Application {
+}
